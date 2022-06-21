6eb7d0908b6f966130eecf25ac4a3a3352af86d1
sheenamkvgroup <sheenam@kvgroupglobal.com>
2022-06-21 10:45:32 +0530

    added order history page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './includes/header/header.component';
 import { FooterComponent } from './includes/footer/footer.component';
 import { HomeComponent } from './includes/home/home.component';
+import { OrderHistoryComponent } from './includes/order-history/order-history.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    OrderHistoryComponent
   ],
   imports: [
     BrowserModule,
