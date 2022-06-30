1bcfb8f9762ad19a801f6e49fa3e46327baf3d71
sheenamkvgroup <104189931+sheenamkvgroup@users.noreply.github.com>
2022-06-30 08:19:14 +0530

    Revert "merge"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './includes/home/home.component';
-import { OrderHistoryComponent } from './includes/order-history/order-history.component';
 
-const routes: Routes = [
-  {
-    path:'',
-    component: HomeComponent
-  },
-
-  {
-    path:'order-history',
-    component: OrderHistoryComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,33 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './includes/header/header.component';
 import { FooterComponent } from './includes/footer/footer.component';
-import { HomeComponent } from './includes/home/home.component';
-import { OrderHistoryComponent } from './includes/order-history/order-history.component';
-import { FormsModule } from '@angular/forms';
-import { CollapseModule } from 'ngx-bootstrap/collapse';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
-import { HttpClientModule } from '@angular/common/http';
-import { InsertService } from './services/insert.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
-    HomeComponent,
-    OrderHistoryComponent
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, 
-    FormsModule,
-    CollapseModule,
-    BrowserAnimationsModule,
-    BsDatepickerModule.forRoot(),
-    HttpClientModule
+    AppRoutingModule
   ],
-  providers: [InsertService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
